3730a13bc276a8317f17f33d170c7724f5d5850c
jake xia <jakexia@Henrys-MacBook-Pro.local>
2020-01-05 15:29:43 -0500

    learned about context object and functions

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ const Koa = require('koa'); //require
 const app = new Koa(); //new koa instance (main object)
 const PORT = 3000;
 
+//add date method to context
+app.context.date = Date(); //add 'date' function to context
+
+//response
 app.use(ctx => {
-  ctx.body = 'Hello World';
+  //use is the main syntax used to add any functions with the server
+  //used to create functions etc, in this case, the function ctx.
+
+  //use state
+  ctx.state.user = 'Jake';
+
+  //print hello world with date
+  //use `` special quotes for template strings
+  ctx.body = `Hello, ${ctx.state.user} on ${ctx.date}`;
 })
 
 app.listen(PORT);
